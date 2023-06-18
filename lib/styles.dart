671209bed26b9c83dc28aa0c9795b854bc0ffca3
Brock Krusemark <kruse331@morris.umn.edu>
2023-06-17 21:32:31 -0500

    styles file for consistency

---
+++ lib/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle kodchasan = GoogleFonts.kodchasan();
+
+Color orange = const Color.fromARGB(255, 209, 129, 68);
+Color green = const Color.fromARGB(255, 1, 19, 23);
+Color grue = const Color.fromARGB(255, 41, 48, 51);
+Color ivory = const Color.fromARGB(255, 253, 243, 224);
