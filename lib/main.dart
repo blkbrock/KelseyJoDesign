40233c0b92230002c9f62de2a4f02d62b6d08882
blkbrock <brockkrusemark@gmail.com>
2023-06-30 17:22:03 -0500

    firebase.initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kelsey_website/home_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyBqrkJNn60NlxUw-04to2A-Lni4Qtry9Cs",
+        appId: "1:498345059836:web:367befa5a96f5ec5d8be79",
+        messagingSenderId: "498345059836",
+        projectId: "kelsey-website-b57c2"),
+  );
   runApp(const MyApp());
 }
 
@@ -18,4 +26,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
